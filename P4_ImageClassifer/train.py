4ff918ca622c3e0e9856597d6b15cd53c81cdaf2
leesper <pascal7718@gmail.com>
2018-09-03 22:42:57 +0800

    make batch size 64

--- P4_ImageClassifer/train.py
+++ P4_ImageClassifer/train.py
@@ -65,7 +65,7 @@ data_transforms = {
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_directory, x), 
     data_transforms[x]) for x in ['train', 'valid', 'test']}
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32, shuffle=True) 
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64, shuffle=True) 
                for x in ['train', 'valid', 'test']}
 
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid', 'test']}
