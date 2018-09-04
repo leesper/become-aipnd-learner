7006826dacf66aeb8b2040aba911f0367d4a45dc
leesper <pascal7718@gmail.com>
2018-09-04 21:22:19 +0800

    cuda

--- P4_ImageClassifer/common.py
+++ P4_ImageClassifer/common.py
@@ -191,6 +191,7 @@ def predict(image_path, model, class_to_idx, is_gpu, topk=5):
     if is_gpu and torch.cuda.is_available():
         model.cuda()
         im.cuda()
+    model.cuda()
     output = model(im)
     output = F.softmax(output, dim=1)
     idx_to_class = {v: k for k, v in class_to_idx.items()}
