fix model, remove softmax

P4_ImageClassifer/train.py
@@ -73,41 +73,13 @@ dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32, 
 
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid', 'test']}
 
-
-
 model = None
 if arch == 'vgg19':
-    # model = common.VGG19FineTune(hidden_units, len(image_datasets['train'].classes))
-    model = models.vgg19(pretrained=True)
-    num_features = model.classifier[0].in_features
+    model = common.VGG19FineTune(hidden_units, len(image_datasets['train'].classes))
 elif arch == 'resnet50':
-    # model = common.Resnet50FineTune(hidden_units, len(image_datasets['train'].classes))
-    model = models.resnet50(pretrained=True)
-    num_features = model.fc.in_features
+    model = common.Resnet50FineTune(hidden_units, len(image_datasets['train'].classes))
 
-for param in model.parameters():
-    param.require_grad = False
-
-classifier = nn.Sequential(OrderedDict([
-    ('fc1', nn.Linear(num_features, 2000, bias=True)),
-    ('relu', nn.ReLU()),
-    ('dropout', nn.Dropout(p=0.5)),
-    ('fc2', nn.Linear(2000, hidden_units)),
-    ('relu', nn.ReLU()),
-    ('dropout', nn.Dropout(p=0.5)),
-    ('fc3', nn.Linear(hidden_units, 102)),
-    ('output', nn.Softmax(dim=1))
-]))
-
-if arch == 'vgg19':
-    model.classifier = classifier
-    # optimizer = optim.SGD(model.classifier.parameters(), lr=learning_rate, momentum=0.9)
-    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
-elif arch == 'resnet50':
-    model.fc = nn.Linear(in_features=2048, out_features=102, bias=True)
-    # optimizer = optim.SGD(model.fc.parameters(), lr=learning_rate, momentum=0.9)
-    optimizer = optim.Adam(model.fc.parameters(), lr=learning_rate)
-
+optimizer = optim.SGD(model.classifier.parameters(), lr=learning_rate, momentum=0.9)
 criterion = CrossEntropyLoss()
 
 print('training model {}'.format(arch))
@@ -126,5 +98,3 @@ checkpoint = {
 
 torch.save(checkpoint, '{}/{}_checkpoint.pth'.format(save_dir, arch))
 print('{}_checkpoint.pth saved in {}'.format(arch, save_dir))
-
-